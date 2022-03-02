9f55f8e934d44ce7587fc776740a99c4703e0541
parnia <parnia.sakhaei@gmail.com>
2022-03-03 02:15:17 +0330

    a

--- login.php
+++ login.php
@@ -24,7 +24,7 @@
   		<button type="submit" class="btn" name="login_user">Login</button>
   	</div>
   	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
+  		Not yet a member? <a href="signup.php">Sign up</a>
   	</p>
   </form>
 </body>

f974a98c792746d0bc9407a24d58c8eaec0e25df
Yashika <96520667+k-Yashika@users.noreply.github.com>
2022-03-02 21:15:14 +0800

    Update rep_server.php

--- rep_server.php
+++ rep_server.php
@@ -1,8 +1,8 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "cityzen");
+$db = mysqli_connect("localhost", "root", "", "cityzen");
 
-if($conn === false){
+if($db === false){
     die("ERROR: Could not connect" . mysqli_connect_error());
 }
 
@@ -12,16 +12,30 @@ $email = $_POST['email'];
 $mobileno = $_POST['mobileno'];
 $jobtitle = $_POST['jobtitle'];
 
-$sql = "INSERT INTO representatives VALUES ('$username', '$fullname', '$email', '$mobileno', '$jobtitle')";
+//generate vkey
+$vkey = md5(time() .$username);
 
-if(mysqli_query($conn, $sql)){
-    echo "data stored successfully";
+$insert = "INSERT INTO representatives VALUES 
+    ('$username', '$fullname', '$email', '$mobileno', '$jobtitle', '$vkey')";
+
+if(mysqli_query($db, $insert)){
+    //send email
+    $to = $email;
+    $subject = "cityZen Email Verification";
+    $message = "<a href='http://localhost/cityzen/verify.php?vkey=$vkey'>Click Here to Verify Yourself as a Representative</a>";
+    $headers = "From: ctznprjct@yahoo.com" . "\r\n";
+    $headers .= "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;Charset=utf8" . "\r\n";
+
+    mail($to,$subject,$message,$headers);
+
+    header('location:new_rep.php');
 } else{
-    echo "ERROR: $sql. " . mysqli_error($conn);
+    echo $db->error;
 }
 
-mysqli_close($conn);
-header("location: new_rep.php");
+mysqli_close($db);
+//header("location: new_rep.php");
 
 //add alert dialog to inform user that rep is added
 

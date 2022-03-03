3b7a05d9661736487f031e35c2cf715e23a9b2d1
Yashika <96520667+k-Yashika@users.noreply.github.com>
2022-03-03 22:25:38 +0800

    Update org_server.php

--- org_server.php
+++ org_server.php
@@ -1,23 +1,24 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "cityzen");
+$db = mysqli_connect("localhost", "root", "", "cityzen");
 
-if($conn === false){
+if(!$db){
     die("ERROR: Could not connect" . mysqli_connect_error());
 }
 
+$org_id = $_POST['org_id'];
 $name = $_POST['name'];
 $address = $_POST['address'];
 
-$sql = "INSERT INTO organizations VALUES ('$name', '$address')";
+$insert = "INSERT INTO organizations VALUES ('$org_id', '$name', '$address')";
 
-if(mysqli_query($conn, $sql)){
-    echo "data stored successfully";
+if(mysqli_query($db, $insert)){
+    echo "Success";
 } else{
-    echo "ERROR: $sql. " . mysqli_error($conn);
+    echo $db->error;
 }
 
-mysqli_close($conn);
+mysqli_close($db);
 header("location: new_org.php");
 
 //add alert dialog to inform user that rep is added

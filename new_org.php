dcf91b6a5c78939fa764ea822a45756a66ae8711
Yashika <k.yashika@outlook.com>
2022-03-07 19:29:56 +0800

    Update new_org.php

--- new_org.php
+++ new_org.php
@@ -5,90 +5,121 @@
 
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <link rel="stylesheet" href="style.css"> -->
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://unpkg.com/@popperjs/core@2">
-        
-        
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+
         <style>
-            body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
-            body, html {
-                height: 100%;
-                line-height: 1.8;
-            }
-    
-            .w3-bar .w3-button{
-                padding: 16px;
-            }
-    
-            .w3-container{
-              margin-top: 50px;
-              padding: 16px;
-            }
-    
-            *{
-                box-sizing: border-box;
-            }
-    
-            .container{
-                background-color: beige;
-                padding: 5px 28px 15px 20px;
-                border: 1px solid lightgray;
-                border-radius: 4px;
-            }
-    
-            input[type="text"]{
-                width: 50%;
-                padding: 12px;
-                border: 1px solid #ccc;
-                border-radius: 5px;
-                margin: 10px;
-            }
-    
-            input[type="submit"]{
-                background-color: #ccc;
-                color: white;
-                padding: 12px 20px;
-                border: none;
-                border-radius: 4px;
-                cursor: pointer;
-                width: 50%;
-            }
-    
-            input[type="submit"]:hover{
-                background-color: #FFC300;
-            }
-            
-            </style>
+          
+      body {
+        background-image: url('img/');
+        background-repeat: no-repeat;
+        background-attachment: fixed; 
+        background-size: 100% 100%;
+      }
+        body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+        body, html {
+            height: 100%;
+            line-height: 1.8;
+            text-align: center;
+        }
+
+        .w3-bar .w3-button {
+            padding: 16px;
+        }
+
+        .w3-container{
+          margin-top: 50px;
+          padding: 16px;
+        }
+
+        input[type="submit"], input[type="text"]{
+          display:block;
+          margin-left: 240px;
+        }
+
+        input[type="text"]{
+            color: black;
+            padding: 12px 20px;
+            border-radius: 4px;
+            cursor: pointer;
+            width: 50%;
+        }
+        input[type="submit"]{
+          background-color: #ccc;
+          color: white;
+          padding: 12px 20px;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+          width: 50%;
+        }
+
+        input[type="submit"]:hover{
+          background-color: #FFC300;
+        }
+        </style>
+    </head>
     </head>
     <body>
 
-    <!-- Navbar (sit on top) -->
-    <div class="w3-top">
-    <div class="w3-bar w3-white w3-card" id="myNavbar">
-        <a href="manage_org.php" class="w3-bar-item w3-button w3-wide">cityZen</a>
-        <!-- Right-sided navbar links -->
-        <div class="w3-right w3-hide-small">
-        <a href="#about" class="w3-bar-item w3-button"><i class="fa fa-user"></i> ABOUT</a>
-        <a href="#manage-organization" class="w3-bar-item w3-button"><i class="fa fa-th"></i> MANAGE ORGANIZATION</a>
-        <a href="#log-out" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> LOG OUT</a>
-        </div>
-
-        <!-- Hide right-floated links on small screens and replace them with a menu icon -->
-        <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
-        <i class="fa fa-bars"></i>
+    <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+      <!--to contain contents in the container in the nav bar-->
+      <div class="container">
+        <!--Put title and image of the website-->
+        <a href="#" class="navbar-brand mb-0 h1">
+          <img src="img/cityZen.png" width="45" height="auto" alt="imageWeb">
+          cityZen
         </a>
-    </div>
-    </div>
+         <!--
+          To toggle the navigation bar
+          data-toggle: class that will be applying toggle to 
+          data-target: target will be that ID created in div tag below
+          add accessible tags: aria-controls, expanded, label
+        -->
+        <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" class="navbar-toggler">
+          <!--Add icon for the toggle button-->
+          <span class="navbar-toggler-icon"></span>
 
-    <!-- Sidebar on small screens when clicking the menu icon -->
-    <nav class="w3-sidebar w3-bar-block w3-black w3-card w3-animate-left w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
-        <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
-        <a href="#manage-organization" onclick="w3_close()" class="w3-bar-item w3-button">MANAGE ORGANIZATION</a>
-        <a href="#account" onclick="w3_close()" class="w3-bar-item w3-button">ACCOUNT</a>
-        <a href="#log-out" onclick="w3_close()" class="w3-bar-item w3-button">LOG OUT</a>
+        </button>
+        <!--to ensure that this tag to have responsive design to make nav bar function properly-->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav">
+            <!--Create dropdown toggle-->
+            <li class="nav-item active dropdown">
+              
+              <!--To dropdown the items-->
+              <a href="manage_org.php" class="nav-link">
+                Manage Organization
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="logout.php" class="nav-link">
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+        
+        <!--Create search bar-->
+        <!--
+          form-control: create some of the stylings for the input
+        
+        <form action="#" class="d-flex">
+          <input type="text" class="form-control me-2" name="search">
+          <button type="submit" class="btn btn-outline-success">
+            Search
+          </button>
+        </form>
+      -->
+      </div>
     </nav>
     <div class="w3-container" id="add-org">
         <h1 class="w3-center">New Organization</h1>
@@ -97,9 +128,10 @@
 
       <form class="orgForm" action="org_server.php" method="POST">
         <div class="w3-center">
+            <label type="text" name="org_id"></p>
             <p>Name <input type="text" name="name" required></p>
-            <p>Address <textarea name="address" cols="100" rows="8"></textarea></p>
-            <input class="submit-btn" type="submit" value="Submit">
+            <p>Address <textarea name="address" cols="100" rows="8" required></textarea></p>
+            <input class="submit" type="submit" value="Submit">
     </div>
     </form>
     </body>
